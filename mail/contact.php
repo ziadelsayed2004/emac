47f57cae3074331d6b6d5b5ef4d1ffcd1a54eaa8
ziadelsyaed2004 <ziadelsayed2004@gmail.com>
2025-05-11 19:59:00 +0300

    new

--- mail/contact.php
+++ mail/contact.php
@@ -1,7 +1,12 @@
 <?php
-if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-  http_response_code(500);
-  exit();
+if (
+    empty($_POST['name']) ||
+    empty($_POST['subject']) ||
+    empty($_POST['message']) ||
+    !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)
+) {
+    http_response_code(400);
+    exit();
 }
 
 $name = strip_tags(htmlspecialchars($_POST['name']));
@@ -10,11 +15,23 @@ $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 $to = "info@emaccon.com";
-$subject = "$m_subject:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
-$header = "From: $email";
-$header .= "Reply-To: $email";	
+$subject = "$m_subject: $name";
 
-if(!mail($to, $subject, $body, $header))
-  http_response_code(500);
+$body = "You have received a new message from your website contact form.\n\n";
+$body .= "Here are the details:\n\n";
+$body .= "Name: $name\n";
+$body .= "Email: $email\n";
+$body .= "Subject: $m_subject\n";
+$body .= "Message:\n$message\n";
+
+$headers = "From: Emaccon Website <info@emaccon.com>\r\n";
+$headers .= "Reply-To: $email\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+if (mail($to, $subject, $body, $headers)) {
+    http_response_code(200);
+} else {
+    http_response_code(500);
+}
 ?>
