cf63ad240e4c233012f1bf6a5a7fce514d7492c2
ziadelsyaed2004 <ziadelsayed2004@gmail.com>
2025-05-11 20:05:10 +0300

    new

--- mail/contact.js
+++ mail/contact.js
@@ -15,7 +15,7 @@ $(function () {
             $this.prop("disabled", true);
 
             $.ajax({
-                url: "contact.php",
+                url: "url:contact/contact.php",
                 type: "POST",
                 data: {
                     name: name,
